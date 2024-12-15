2c6348b9daaae93d1673c34633177293d7d7d518
David Junior Phiri <davidphiri242@gmail.com>
2024-12-15 13:18:08 +0200

    Delete django-models/django-models directory

--- django-models/django-models/relationship_app/query_samples.py
+++
@@ -1,8 +0,0 @@
-#Query all books by a specific author
-books = Book.objects.filter(author= author_name)
-
-#List all books in a library
-list_books = Library.objects.all()
-
-#Retrieve the librarian for a library
-librarian_for_library = Librarian.objects.get(library = 'Retreat')
