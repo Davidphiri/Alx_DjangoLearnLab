2ec1255bf74263a1119901153cfca9e8f6e2f38b
David Junior Phiri <davidphiri242@gmail.com>
2025-01-02 14:00:09 +0200

    DeleLibraryProject directory

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-class UserAdmin(BaseUserAdmin):
-    list_display = ['email','username','is_staff']
-    
-admin.site.register(UserAdmin)
-
