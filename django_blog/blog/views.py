7b4f1bf3e0002723abd46acc7db1857694c17cfc
David Junior Phiri <davidphiri242@gmail.com>
2025-01-11 14:14:49 +0200

    Add files via upload

---
+++ django_blog/blog/views.py
@@ -0,0 +1,30 @@
+
+
+# Create your views herefrom django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+
+# Registration view
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'auth/register.html', {'form': form})
+
+# Profile view
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm(instance=request.user)
+    return render(request, 'auth/profile.html', {'form': form})
