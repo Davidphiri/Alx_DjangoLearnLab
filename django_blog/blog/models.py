705f6bac01db9b28f7214ddfdaa1aa201a41b84c
David Junior Phiri <davidphiri242@gmail.com>
2025-01-11 15:14:43 +0200

    Add files via upload

---
+++ django_blog/blog/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    tags = TaggableManager() 
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
